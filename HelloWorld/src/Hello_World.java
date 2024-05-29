b5ea8dbf1f570f34aeb380c0284bc1999ddba21d
Nathan Chapman <nchap21@gmail.com>
2024-05-29 09:41:27 -0600

    first commit

---
+++ HelloWorld/src/Hello_World.java
@@ -0,0 +1,10 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("csc1060 summer class");
+	}
+
+}
