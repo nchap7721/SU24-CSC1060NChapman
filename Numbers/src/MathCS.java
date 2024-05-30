4e6300051fa49eb60c3c74e1a34c8bca29bea1a8
Nathan Chapman <nchap21@gmail.com>
2024-05-30 16:16:53 -0600

    tempConverter

---
+++ Numbers/src/MathCS.java
@@ -0,0 +1,31 @@
+/* MathCS
+ * Nathan Chapman
+ * 5/29/2024
+ */
+public class MathCS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 15;
+		int num2 = 7;
+		int num3 = num1 + num2;
+		//these exist because they were initialized just like the doubles and floats
+		
+		//float fnum = 10.0f; this is a floating point number
+		
+		
+		System.out.println("the total is " + num3);
+		
+		float num4 = 1.1f;
+		float num5 = 2.2f;
+		float num6 = num5 + num4;
+		System.out.println("the total is " + num6);
+		
+		double num7 = 2.36;
+		double num8 = 5.68;
+		double num9 = num8 + num7;
+		System.out.println("the total is " + num9);
+	}
+
+}
+//problems:
---
+++ TemperatureConverter/src/TempConverter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TempConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double faren; 
+		double cel;
+		double kel;
+		System.out.println("Give me a temperaure in farenheit and I'll give it to you in Celsius and Kelvin");
+		Scanner scn = new Scanner(System.in);
+		faren = scn.nextInt();
+		cel = ((faren - 32)*5)/9;
+		kel = cel + 273.15;
+		System.out.println("you gave me " + faren + " degrees is Farenheit\nyour temperature in Celsius is " + 
+		cel + " degrees\nyour temperature in Kelvin is " + kel + " degrees");
+	}
+
+}
