932b2dd7f6e6b46c5e44d20d5cc2ba16842d7756
Nathan Chapman <nchap21@gmail.com>
2024-07-03 14:04:55 -0600

    array reverse

--- ArrayReversal/src/ArrayReversal.java
+++ ArrayReversal/src/ArrayReversal.java
@@ -1,9 +1,31 @@
 
+import java.util.Scanner;
+
 public class ArrayReversal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+
+		int[] numbers = new int[100];
+		int count = 0;
+
+		System.out.println("Enter positive numbers and when you're done enter -1");
+		boolean check = true;
+		while (check == true) {
+			int num = scn.nextInt();
+			if (num == -1 || count >= 100) {
+				check = false;
+				break;
+			}
+			numbers[count] = num;
+			count++;
+		}
+
+		System.out.println("Your numbers in reverse order:");
+		for (int i = count - 1; i >= 0; i--) {
+			System.out.println(numbers[i]);
+		}
 
 	}
-
 }
